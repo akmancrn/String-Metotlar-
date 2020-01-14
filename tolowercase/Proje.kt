2acdbcde919d4da92c8fb4908cf3b89581e4f31c
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-14 16:57:21 +0300

    Create Proje.kt

---
+++ tolowercase/Proje.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import java.io.PrintStream
+
+fun main (args : Array<String>)
+{
+
+   val str = "Yeni Başlayanlar için Kotlin ve Android Programlama"
+
+   print(str.toLowerCase())
+}
