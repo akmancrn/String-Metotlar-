61e81f6fb62d27ec1790aa23d324283899fc4f78
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:24:58 +0300

    Create Proje.kt

---
+++ empty/Proje.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import java.io.PrintStream
+
+fun main (args : Array<String>)
+{
+
+   val str = "Yeni Başlayanlar için Kotlin ve Android Programlama"
+   val str1 = ""
+   println(str.isEmpty())
+   println(str1.isEmpty())
+}
