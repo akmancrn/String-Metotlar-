0aec33b070b854bc4abaed761f84de82161d9472
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:26:43 +0300

    Create Proje.kt

---
+++ replace/Proje.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import java.io.PrintStream
+
+fun main (args : Array<String>)
+{
+
+   val str = "Yeni Başlayanlar için Kotlin ve Android Programlama"
+
+   println(str.replace("Programlama", "Kodlama"))
+
+}
