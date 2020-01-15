218564a1deaa665aa00e5432c7aab0ada571a0be
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:19:52 +0300

    Proje.kt

---
+++ equals/Proje.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import java.io.PrintStream
+
+fun main (args : Array<String>)
+{
+
+   val str = "Yeni Başlayanlar için Kotlin ve Android Programlama"
+
+   println(str.equals("Yeni Başlayanlar için Kotlin ve Android Programlama"))
+   println(str.equals("Kotlin ve Android Programlama"))
+}
